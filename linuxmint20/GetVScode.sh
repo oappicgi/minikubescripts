2d93a3131e77e79877ecf7aec55d4ffab18060f8
oappicgi <ossi.ala-peijari@cgi.com>
2020-09-12 23:34:28 +0300

    first scripts to linux mint 20

---
+++ linuxmint20/GetVScode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update  -y
+sudo apt install apt-transport-https -y
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ 
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update -y
+sudo apt install code -y
+KUBE_EDITOR='code -w' //set this as enviornmental variable
---
+++ linuxmint20/getMiniKube.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get -y install docker-compose
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+sudo apt-get install conntrack -y
