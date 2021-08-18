c118f18672ba2acc1e234ff22bbeb4c26e57e8a4
oappicgi <ossi.ala-peijari@cgi.com>
2021-08-18 22:02:49 +0300

    added comment to VS install script

--- linuxmint20/GetVScode.sh
+++ linuxmint20/GetVScode.sh
@@ -6,6 +6,6 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update -y
 sudo apt install code -y
-KUBE_EDITOR='code -w' //set this as enviornmental variable
+KUBE_EDITOR='code -w' #set this as enviornmental variable
 npm install -g eslint
 
