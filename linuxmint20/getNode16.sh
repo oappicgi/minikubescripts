e7f49686e1648abdf42ccaaec670982ccd6eb9fd
oappicgi <ossi.ala-peijari@cgi.com>
2021-11-17 22:42:59 +0200

    combined node and getcdk

--- linuxmint20/getNode16.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs
--- linuxmint20/getcdk.sh
+++ linuxmint20/getcdk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt install python3-pip -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
 sudo npm -g install typescript
 sudo npm install -g aws-cdk
