9a1d1d8fc420a86667263f31441dbf5293a384e6
oappicgi <ossi.ala-peijari@cgi.com>
2021-11-17 19:56:38 +0200

    added install all and cleanup

--- linuxmint20/getVSExtensions.sh
+++ linuxmint20/getVSExtensions.sh
@@ -5,7 +5,3 @@ code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension AmazonWebServices.aws-toolkit-vscode
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dbaeumer.vscode-eslint
-
