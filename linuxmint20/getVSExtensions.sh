f6b6251e11b8b021f4a8d52cce56aec6885821e4
oappicgi <ossi.ala-peijari@cgi.com>
2021-11-17 19:48:34 +0200

    extensions and aws tools

---
+++ linuxmint20/getVSExtensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension aaron-bond.better-comments
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension AmazonWebServices.aws-toolkit-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
+
